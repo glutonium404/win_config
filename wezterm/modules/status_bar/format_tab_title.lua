03b2453397ab4ae4745ecd98b1c50a42deaa21dd
glutonium69 <tahzib404@gmail.com>
2026-08-16 03:21:28 +0600

    Randomly choose a color pallete from a set of palettes

--- wezterm/modules/status_bar/format_tab_title.lua
+++ wezterm/modules/status_bar/format_tab_title.lua
@@ -1,19 +1,27 @@
 local wezterm = require "wezterm"
 
-local palettes = {
+---@class ColorPalette
+---@field palettes string[][]
+---@field current_palette string[]?
+
+---@type ColorPalette
+local M = {
+  palettes = {
     { '#849dc4', '#80c49d', '#ffd780', '#f28fad', '#E5D1FA' },
     { '#E6F2DD', '#ffd780', '#B1D3B9', '#88BDA4', '#659287' },
     { '#FFD6A6', '#FFF0BE', '#FFB399', '#FF9A86', '#80c49d' },
     { '#FFD1DA', '#FBF3D1', '#DEDED1', '#C5C7BC', '#B6AE9F' },
     { '#ECF9FF', '#C4E1E6', '#A4CCD9', '#8DBCC7', '#EBFFD8' },
     { '#E5D1FA', '#FFDBAA', '#FFD1DA', '#F1F0E8', '#FFB399' },
-  }
+  },
+}
 
-local palette = palettes[math.random(#palettes)]
+M.current_palette = M.palettes[math.random(#M.palettes)]
 
 wezterm.on('format-tab-title', function(tab, _, _, _, _, _)
-  local color = palette[(tab.tab_index % #palette) + 1]
+  local color = M.current_palette[(tab.tab_index % #M.current_palette) + 1]
   local index = tab.tab_index + 1
+  local working_dir = tab.active_pane.foreground_process_name
 
   if tab.is_active then
     return {
@@ -35,3 +43,5 @@ wezterm.on('format-tab-title', function(tab, _, _, _, _, _)
     }
   end
 end)
+
+return M
--- wezterm/modules/status_bar/status_bar.lua
+++ wezterm/modules/status_bar/status_bar.lua
@@ -1,17 +1,28 @@
+local wezterm = require("wezterm")
+
 local M = {}
 
 require("modules.status_bar.update_status")
-require("modules.status_bar.format_tab_title")
+local p = require("modules.status_bar.format_tab_title") ---@type ColorPalette
 
 ---@param config Config
 function M.apply_to_config(config)
   config.tab_bar_at_bottom = false
   config.use_fancy_tab_bar = false
-  config.show_new_tab_button_in_tab_bar = false
-
+  config.show_new_tab_button_in_tab_bar = true
+  config.hide_tab_bar_if_only_one_tab = true
   config.colors = config.colors or {}
 
   config.colors.tab_bar = { background = 'none' }
+
+  config.tab_bar_style = {
+    new_tab = wezterm.format {
+      { Background = { Color = 'none' } },
+      { Foreground = { Color = p.current_palette[1] } },
+      { Text = ' [[+]] ' },
+    },
+    new_tab_hover = ""
+  }
 
   -- add physical space directly above the top tab line
   config.window_frame = config.window_frame or {}
